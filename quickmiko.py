7a025f09bb03fe15b4e8062904acb93435a54bd8
MonoSlush <traxusiv@gmail.com>
2020-07-21 09:03:49 -0700

    Updating quickmiko

--- quickmiko.py
+++ quickmiko.py
@@ -11,7 +11,37 @@ nxos1 = {
 'device_type': 'cisco_nxos',
 }
 
-net_connect = ConnectHandler(**nxos1)
-print(net_connect.find_prompt())
+nxos2 = {
+'host': 'nxos2.lasthop.io',
+'username': 'pyclass',
+'password': password,
+'device_type': 'cisco_nxos',
+}
 
+
+connection_list = [nxos1, nxos2]
+for device in connection_list:
+    net_connect = ConnectHandler(**device)
+    print(net_connect.find_prompt())
+
+
+# net_connect = ConnectHandler(**nxos1)
+# print(net_connect.find_prompt())
+
+cisco3 = {
+'host': 'cisco3.lasthop.io',
+'username': 'pyclass',
+'password': password,
+'device_type': 'cisco_ios',
+}
+
+show_version = 'show version'
+
+cisco3_connect = ConnectHandler(**cisco3)
+
+
+
+
+print(cisco3_connect.find_prompt())
+print(cisco3_connect.send_command(show_version))
 
